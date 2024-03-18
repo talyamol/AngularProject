b5e53082834189776f723a89e67cf11c435178eb
talyamol <talyamolson@gmail.com>
2024-03-19 01:18:06 +0200

    second

--- src/app/course/all-courses/all-courses.component.ts
+++ src/app/course/all-courses/all-courses.component.ts
@@ -14,176 +14,86 @@ import { CategoryServerService } from '../../category-server.service';
   styleUrl: './all-courses.component.css'
 })
 
-export class AllCoursesComponent implements OnInit, OnDestroy {
+export class AllCoursesComponent implements OnInit {
   
   public createdByUsername: string;
   public rows: Courses[][] = [];
   public selectCourses: Courses;
   public allCourses: Courses[];
-  public filterCourses1: Courses[] = [];
+  //public filterCourses1: Courses[] = [];
   public allCategories:Category[];
   sidebarVisible: boolean = false;
+  public filteredProductsList: Courses[];
+  public filters: { [key: string]: string } = {};
+  showFilters: boolean = false;
 
 
-  private refreshSubscription: Subscription;
-  public value: number = 0; // Difficulty level filter value
-  public kind: KindCourse; // Preparation time filter value
-  public selectedCategories: number[] = []; // Selected categories for filtering
-  public sidebarVisible2: boolean = false;
-  public disabled = false;
-  public max = 5;
-  public min = 0;
-  public showTicks = true;
-  public step = 0;
-  public thumbLabel = true;
-  public courseNameFilter: string = '';
 
   constructor(private router: Router, private route: ActivatedRoute, private _courseServise: CoursesServerService,private _categoriesService:CategoryServerService ) { }
 
   ngOnInit(): void {
-    this.loadData();
-    this.setupDataRefresh();
-  }
+    // this.loadData();
 
-  ngOnDestroy(): void {
-    if (this.refreshSubscription) {
-      this.refreshSubscription.unsubscribe();
-    }
-  }
+    this._categoriesService.getAllCaregory().subscribe(res=>{
+      this.allCategories=res;
+    });
 
-  private loadData(): void {
     this._courseServise.getAllCourses().subscribe(res => {
       this.allCourses = res;
-      this.filterCourses1 = [...res];
+     this.filteredProductsList=this.allCourses
       console.log(this.allCourses);
     });
   }
+  // private loadData(): void {
+   
+  // }
 
-  private setupDataRefresh(): void {
-    this.refreshSubscription = interval(60000).subscribe(() => {
-      this.loadData();
-    });
-  }
-  toggleSidebar() {
-    this.sidebarVisible = !this.sidebarVisible;
-}
+
   public courses(c: Courses): void {
     this.selectCourses = c;
     console.log(c.id, "id");
 
     this.router.navigate(["/course/course-details", c.id]);
   }
+  toggleFilters() {
+    this.showFilters = !this.showFilters;
+  }
 
-  public filterCourses(): void {
-    this.filterCourses1 = this.allCourses.filter(course => {
-      const kindCourseFilter = this.kind === 0 || course.kindCourse === this.kind;
-      const categoryFilter = this.selectedCategories.length === 0 || this.selectedCategories.includes(course.categoryId);
-      const nameFilter = this.courseNameFilter === '' || course.name.toLowerCase().includes(this.courseNameFilter.toLowerCase());
-      return kindCourseFilter && categoryFilter && nameFilter;
+  hideFilters() {
+    this.showFilters = false;
+  }
+
+
+
+  filterCourses(filterType: string, event: any): void {
+    const value = event.target.value;
+    this.filters[filterType] = value;
+    if (value === '') {
+      // אם הערך הנבחר הוא ריק, אז אין צורך בפילטר
+      delete this.filters[filterType]; // מחיקת הפילטר מהמערך
+    }
+    this.applyFilters();
+    console.log(this.filteredProductsList);
+  }
+
+  applyFilters(): void {
+    this.filteredProductsList = this.allCourses.filter(course => {
+      return Object.keys(this.filters).every(key => {
+        const value = this.filters[key].toLowerCase();
+        switch (key) {
+          case 'courseName':
+            return course.name && course.name.toLowerCase().includes(value);
+          case 'learningWay':
+            return course.kindCourse?.toString() === value;
+          case 'categoryId':
+            return course.categoryId?.toString() === value;
+          default:
+            return true;
+        }
+      });
     });
   }
 
-  public filterByName(): void {
-    this.filterCourses();
-  }
-  public filterByCategory(category: number, event: any): void {
-    if (event.target.checked) {
-      this.selectedCategories.push(category);
-    } else {
-      const index = this.selectedCategories.indexOf(category);
-      if (index !== -1) {
-        this.selectedCategories.splice(index, 1);
-      }
-    }
-    this.filterCourses();
-  }
-  public categories() {
-    this._categoriesService.getAllCaregory().subscribe(res=>{
-        this.allCategories = res;
-        console.log(this.allCategories);
-      });
-}}
-
-// export class AllCoursesComponent implements OnInit{
-  
-//   public createdByUsername: string;
-//   public rows: Courses[][] = [];
-//   public selectCourses: Courses;
-//   public allCourses: Courses[];
-//    filterCourses1:Courses[]=[];
-//   private refreshSubscription: Subscription;
-//   value: number = 0; // Difficulty level filter value
-//   kind:KindCourse // Preparation time filter value
-//   selectedCategories: number[] = []; // Selected categories for filtering
-//   sidebarVisible2: boolean = false;
-//   disabled = false;
-//   max = 5;
-//   min = 0;
-//   showTicks = true;
-//   step = 0;
-//   thumbLabel = true;
-//   courseNameFilter: string = '';
-
-//   constructor(private router: Router,private route: ActivatedRoute, private _courseServise: CoursesServerService) { }
-//   ngOnInit(): void {
-//     this.loadData(); // טען את המידע בהתחלה
-//     this.setupDataRefresh(); // הפעל רענון אוטומטי של המידע
-//   }
 
 
-//   ngOnDestroy(): void {
-//     // הפסק לרענן את המידע כאשר הקומפוננטה נסגרת
-//     if (this.refreshSubscription) {
-//       this.refreshSubscription.unsubscribe();
-//     }
-//   }
-
-//   private loadData(): void {
-//     this._courseServise.getAllCourses().subscribe(res=>{
-//         this.allCourses = res;
-//         this.filterCourses1=[...res];
-//         console.log(this.allCourses);
-//       });
-//   }
-
-//   private setupDataRefresh(): void {
-//     // הפעל פונקציה שמרעינה את המידע בקביעות
-//     this.refreshSubscription = interval(60000).subscribe(() => {
-//       this.loadData();
-//     });
-//   }
-
-//   public courses(c: Courses): void {
-//     this.selectCourses = c;
-//     console.log( c.id,"id");
-    
-//     this.router.navigate(["/course/course-details", c.id]);
-//   }
-
-
-//   filterCourses(): void {
-//     this.filterCourses1 = this.allCourses.filter(course => {
-//         const kindCourseFilter = this.kind === 0 || course.kindCourse === this.kind;
-//         const categoryFilter = this.selectedCategories.length === 0 || this.selectedCategories.includes(course.categoryId);
-//         const nameFilter = this.courseNameFilter === '' || course.name.toLowerCase().includes(this.courseNameFilter.toLowerCase());
-//         return kindCourseFilter && categoryFilter && nameFilter;
-//     });
-//     this.filterByCategory(); // קריאה לפונקציה לסינון לפי הקטגוריה בכל פעם שהוא מתבצע
-//   }
-  
-//   filterByName(): void {
-//     this.filterCourses(); // קריאה לפונקציה לסינון הכללי בכל פעם שהוא מתבצע
-//   }
-  
-//   filterByCategory(category: number = null): void {
-//     if (category !== null) {
-//       const index = this.selectedCategories.indexOf(category);
-//       if (index !== -1) {
-//         this.selectedCategories.splice(index, 1);
-//       } else {
-//         this.selectedCategories.push(category);
-//       }
-//     }
-//     this.filterCourses(); // קריאה לפונקציה לסינון הכללי בכל פעם שהוא מתבצע
-//   }
-// }
+}
